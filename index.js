26e7337c2027bbba466c37141381aeedb6aeb17c
Geovane Silva <silvafranco71@gmail.com>
2024-11-30 19:31:29 -0300

    Enviando ultimas alteracoes da pagina contato

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 for (let i = 1; i <= 20; i++) {
   console.log(`Executando laço de repetição ${i} vezes...`);
 }
+
+function handleHello(name) {
+  console.log(`Olá, seja bem vindo ${name}!!!`);
+}
+
+handleHello("Geovane Silva");
